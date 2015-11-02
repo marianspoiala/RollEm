7363acfcaf3910b786104027f707f4f0234ae4a1
Gavril Alexandru <alexandru_florin.gavril@cti.pub.ro>
2015-11-02 22:16:05 +0200

    Initial Commit - Unity Project
    Am adaugat toate resursele Unity + Proiectul de C#.
    Realizat animatia pentru aruncarea random a zarurilor.

---
+++ MPS Project/Assets/3DDiceRoller/ApplyForceInRandomDirection.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ApplyForceInRandomDirection : MonoBehaviour {
+
+    public ForceMode forceMode;
+    public float ForceAmount = 10.0f;
+    public float AngularForceAmount = 10.0f;
+	// Use this for initialization
+	void Start () {
+        Vector3 force = new Vector3((Random.value-0.5f) * ForceAmount, 
+            0, 
+            (Random.value - 0.5f) * ForceAmount);
+
+        GetComponent<Rigidbody>().AddForce(force, forceMode);
+        GetComponent<Rigidbody>().AddTorque(Random.onUnitSphere, forceMode);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKeyDown("space"))
+        {
+
+        }
+	}
+}
