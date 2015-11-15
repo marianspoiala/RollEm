93fecd009ddaea1ff159be4b3c4c4cb24d054dfa
Marian Spoiala <spoialamarian@gmail.com>
2015-11-15 12:31:43 +0200

    Added functional words list

--- MPS Project/Assets/3DDiceRoller/Scripts/Buttons.cs
+++ MPS Project/Assets/3DDiceRoller/Scripts/Buttons.cs
@@ -2,10 +2,26 @@
 using System.Collections;
 using UnityEngine.UI;
 
+//This script is used for every button and input on the scene
 public class Buttons : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    GameObject startBtn;
+    GameObject wordBtn;
+    GameObject wordInput;
+    GameObject userNameInput;
+    GameObject wordsListView;
+    GameObject wordsListText;
+
+    // Use this for initialization
+    void Start () {
+
+        //Initialize buttons and inputs
+        startBtn = GameObject.Find("StartBtn");
+        wordBtn = GameObject.Find("WordBtn");
+        wordInput = GameObject.Find("WordInput");
+        userNameInput = GameObject.Find("UserNameInput");
+        wordsListView = GameObject.Find("WordsListView");
+        wordsListText = GameObject.Find("WordsListText");
     }
 	
 	// Update is called once per frame
@@ -13,29 +29,56 @@ public class Buttons : MonoBehaviour {
 	    
 	}
 
+    //When object is enabled (at start or setActive(true))
     void OnEnable()
     {
-        Button startButton = GetComponent<Button>();
-        if (startButton.name.Equals("StartBtn"))
+        //Adding listeners to button
+        Button button = GetComponent<Button>();
+        if (button.name.Equals("StartBtn"))
         {
-            startButton.onClick.AddListener(StartFunction);
+            button.onClick.AddListener(StartFunction);
         }
-        else if (startButton.name.Equals("WordBtn"))
+        else if (button.name.Equals("WordBtn"))
         {
-            startButton.onClick.AddListener(WordFunction);
+            button.onClick.AddListener(WordFunction);
         }
 
     }
 
     void StartFunction()// your listener calls this function
     {
-        InputField userName = GameObject.Find("UserNameInput").GetComponent<InputField>();
+        InputField userName = userNameInput.GetComponent<InputField>();
         print("Start game! " + userName.text);
+
+        //Activate word input and button
+        wordBtn.SetActive(true);
+        wordInput.SetActive(true);
+        wordsListView.SetActive(true);
+        //Resetting words from words list view
+        wordsListText.GetComponent<Text>().text = "";
+
+        //Disable start button and user name input after clicking Start Game
+        startBtn.SetActive(false);
+        startBtn.GetComponent<Button>().onClick.RemoveAllListeners();
+        userNameInput.SetActive(false);
     }
 
     void WordFunction()// your listener calls this function
     {
-        InputField word = GameObject.Find("WordInput").GetComponent<InputField>();
+        InputField word = wordInput.GetComponent<InputField>();
+
+        //Adding word to words list view on GUI
+        if (wordsListText.GetComponent<Text>().text == "")
+        {
+            wordsListText.GetComponent<Text>().text = word.text;
+        }
+        else
+        {
+            wordsListText.GetComponent<Text>().text += "\n" + word.text;
+        }
+
+        //Sending word to MainCanvas to be saved in file
+
         print("Word enter button pressed! " + word.text);
     }
 }
--- MPS Project/Assets/3DDiceRoller/Scripts/MainCanvas.cs
+++ MPS Project/Assets/3DDiceRoller/Scripts/MainCanvas.cs
@@ -4,8 +4,26 @@ using UnityEngine.UI;
 
 public class MainCanvas : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    GameObject startBtn;
+    GameObject wordBtn;
+    GameObject wordInput;
+    GameObject userNameInput;
+    GameObject wordsListView;
+
+    // Use this for initialization
+    void Start () {
+        //Initialize buttons and inputs
+        startBtn = GameObject.Find("StartBtn");
+        wordBtn = GameObject.Find("WordBtn");
+        wordInput = GameObject.Find("WordInput");
+        userNameInput = GameObject.Find("UserNameInput");
+        wordsListView = GameObject.Find("WordsListView");
+
+        //Deactivate word input and button when game launches
+        wordBtn.SetActive(false);
+        wordBtn.GetComponent<Button>().onClick.RemoveAllListeners();
+        wordInput.SetActive(false);
+        wordsListView.SetActive(false);
     }
 	
 	// Update is called once per frame
